c94c5fb85560e55bdba007fb098828b2091e8a9c
JULIAN ANDRES SANTANA JULIO <jsantanaj@tecnocomfenalco.edu.co>
2023-09-26 11:49:43 -0500

    arreglado mat table con carrito y localstorage

--- src/app/pages/visualizar-productos/visualizar-productos.component.ts
+++ src/app/pages/visualizar-productos/visualizar-productos.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DataProductsService } from 'src/app/services/data-products.service';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Productos } from 'src/app/Interfaces/productos';
-
+import { ProductsServicesService } from 'src/app/services/products-services.service';
 @Component({
   selector: 'app-visualizar-productos',
   templateUrl: './visualizar-productos.component.html',
   styleUrls: ['./visualizar-productos.component.css']
 })
-export class VisualizarProductosComponent {
+export class VisualizarProductosComponent implements OnInit{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(public data: DataProductsService) { }
 
   displayedColumns: string[] = ['codigo', 'articulo', 'laboratorio', 'cantidad'];
+
+  dataSource = new MatTableDataSource<Productos>([]); // Inicializa con un arreglo vacío
+
+  ngOnInit() {
+    // No configures el dataSource aquí, solo inicializa con un arreglo vacío
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    // Después de configurar el paginador y el ordenamiento, puedes actualizar los datos
+    this.dataSource.data = this.data.selectedData;
+  }
+
   
 }
