36c6a994e15530020ee6eb6b936a040e429704fb
Andremxsrs <ronnyandres120@gmail.com>
2023-11-19 19:32:15 -0500

    create new module (homePharma) and changes in rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,16 +13,19 @@ import { HistorialMensajesComponent } from './pages/historial-mensajes/historial
 import { ConfirmedProductsComponent } from './pages/confirmed-products/confirmed-products.component';
 import { FinishedProductsComponent } from './pages/finished-products/finished-products.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
+import { HomePharmaComponent } from './pages/home-pharma/home-pharma.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/prueba-login' },
-  { path: 'prueba-login', component: PruebaLoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomePharmaComponent },
+
   
   {
     path: 'menu',
     component: MenuComponent, // Usa el mismo componente principal para mantener el sidebar
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'dashboard' }, // Redirige a 'register' por defecto en '/menu'
+      { path: 'prueba-login', component: PruebaLoginComponent },
       { path : 'dashboard', component: DashboardComponent},
       { path: 'register', component: RegisterSalesComponent }, // Componente RegisterSalesComponent se carga en la vista principal de '/menu'
       { path: 'cambiar-contrasena', component: CambiarContrasenaComponent }, // Componente CambiarContrasenaComponent se carga en la vista principal de '/menu'
---
+++ src/app/pages/home-pharma/home-pharma.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-pharma',
+  templateUrl: './home-pharma.component.html',
+  styleUrls: ['./home-pharma.component.css']
+})
+export class HomePharmaComponent {
+
+}
