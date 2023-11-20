48133e2e60964ba6f2d11e5e1d3900ed9b6c6bc8
unknown <stirwincastro@gmail.com>
2023-11-19 20:47:02 -0500

    navbar casi listo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    
     colors:{
       azul:{
         200: '#7688B5',
         500:'#009EFA'
       }
-    }
+    },
+    boxShadow:{
+      azulbs: '1px 8px 30px 0px rgba(0, 158, 250, 0.70)'
+    },
 
   },
   plugins: [
