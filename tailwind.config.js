aea65afd1506c9b672515eb67ba969b312407988
unknown <stirwincastro@gmail.com>
2023-11-20 22:49:35 -0500

    section 1 casi terminada

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,16 @@ module.exports = {
     boxShadow:{
       azulbs: '1px 8px 30px 0px rgba(0, 158, 250, 0.70)'
     },
+    border:{
+      card:'0.602px solid var(--White, #FFF)'
+    },
+  
+    backgroundImage:{
+      card:' linear-gradient(137deg, rgba(255, 255, 255, 0.80) 5.59%, rgba(255, 255, 255, 0.37) 115.37%)'
+    },
+    backdropBlur:{
+      card:' blur(4.214146614074707px)'
+    }
 
   },
   plugins: [
