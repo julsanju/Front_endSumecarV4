71c6abebaf7579cc5d242657cd869c4181894151
JULIANSANTANA <JULIANSANTANA@DESKTOP-225KJQB>
2024-10-09 16:22:55 -0500

    nuevos cambios para la configuracion, nuevos redireccionamientos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ import { ProductosSalesComponent } from './pages/productos-sales/productos-sales
 import { Page404Component } from './pages/page404/page404.component';
 import { CrearUsuarioComponent } from './pages/crear-usuario/crear-usuario.component';
 
-import { LoginAditionalComponent } from './pages/login-aditional/login-aditional.component';
+// import { LoginAditionalComponent } from './pages/login-aditional/login-aditional.component';
 import { EmailConfirmationComponent } from './pages/email-confirmation/email-confirmation.component';
 import { DocumentHistoryComponent } from './pages/document-history/document-history.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
@@ -20,7 +20,7 @@ export const routes: Routes = [
     { path: 'menu',component: MenuComponent, outlet: 'menu' },
     { path: 'email',component: EmailConfirmationComponent},
     { path: 'prueba-login', component: PruebaLoginComponent },
-    { path: 'login-aditional', component: LoginAditionalComponent },
+    // { path: 'login-aditional', component: LoginAditionalComponent },
     
     {
       path: 'menu',
@@ -36,9 +36,12 @@ export const routes: Routes = [
         {path: 'productos', component: ProductosSalesComponent},
         {path: 'usuarios', component: CrearUsuarioComponent},
         {path: 'documentos', component: DocumentHistoryComponent},
-        {path: 'userProfile', component: UserProfileComponent}
+        {path: 'userProfile', component: UserProfileComponent},
+        
       ],
     },
+
+    
     
     { path: '**', pathMatch : 'full', component: Page404Component},
   ];
